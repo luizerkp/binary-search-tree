1339286d32518950add18ac1553954b083ef8a52
Luis Tamarez <luistamarez@hotmail.com>
2023-01-18 20:06:51 -0800

    Change file name binarytTree.js to binaryTree.js   * Add find method to Tree class   * Add levelOrder method to Tree class   * Add inOrder method to Tree class   * Add preOrder method to Tree class   * Add postOrder medod to Tree class

---
+++ src/binaryTree.js
@@ -0,0 +1,227 @@
+import mergeSort from "./mergeSort";
+import removeDuplicates from "./removeDuplicates";
+import Node from "./nodeClass";
+
+const buildTree = (arr) => {
+  const uniqueValuesArr = removeDuplicates(arr);
+  const sortedArr = mergeSort(uniqueValuesArr);
+  const sortedArrToBST = (start, end) => {
+    if (start > end) {
+      return null;
+    }
+    const mid = start + Math.floor((end - start) / 2);
+    const newNode = new Node(sortedArr[mid]);
+    newNode.leftNode = sortedArrToBST(start, mid - 1);
+    newNode.rightNode = sortedArrToBST(mid + 1, end);
+
+    return newNode;
+  };
+  const root = sortedArrToBST(0, sortedArr.length - 1);
+  return root;
+};
+
+export class Tree {
+  constructor(arr) {
+    this.root = buildTree(arr);
+  }
+
+  insertNode(value, currentNode = this.root) {
+    if (currentNode === null) {
+      return new Node(value);
+    }
+    if (currentNode.data === value) {
+      return currentNode;
+    }
+    if (value < currentNode.data) {
+      currentNode.leftNode = this.insertNode(value, currentNode.leftNode);
+    }
+    if (value > currentNode.data) {
+      currentNode.rightNode = this.insertNode(value, currentNode.rightNode);
+    }
+    return currentNode;
+  }
+
+  deleteNode(value, currentNode = this.root) {
+    if (currentNode === null) {
+      return currentNode;
+    }
+
+    if (currentNode.data === value) {
+      if (currentNode.leftNode === null) {
+        return currentNode.rightNode;
+      }
+      if (currentNode.rightNode === null) {
+        return currentNode.leftNode;
+      }
+      // find smallest value node on the right subtree (inorder sucessor) and assign it to the current node
+      currentNode.data = this.#findMinValue(currentNode.rightNode);
+      // delete the inorder sucessor
+      currentNode.rightNode = this.deleteNode(currentNode.data, currentNode.rightNode);
+    } else {
+      if (value < currentNode.data) {
+        currentNode.leftNode = this.deleteNode(value, currentNode.leftNode);
+      }
+
+      if (value > currentNode.data) {
+        currentNode.rightNode = this.deleteNode(value, currentNode.rightNode);
+      }
+    }
+
+    return currentNode;
+  }
+
+  find(value, currentNode = this.root) {
+    if (currentNode !== null && currentNode.data !== value) {
+      if (value < currentNode.data) {
+        return this.find(value, currentNode.leftNode);
+      }
+
+      if (value > currentNode.data) {
+        return this.find(value, currentNode.rightNode);
+      }
+    }
+
+    return currentNode === null ? "Not Found" : currentNode;
+  }
+
+  levelOrder(callBack) {
+    const levelOrderNodeList = [];
+
+    const levelOrderRecursive = (currentQueue) => {
+      if (currentQueue.length === 0) {
+        return;
+      }
+
+      const currentNode = currentQueue.shift();
+
+      if (callBack) {
+        callBack(currentNode);
+      } else {
+        levelOrderNodeList.push(currentNode.data);
+      }
+
+      if (currentNode.leftNode) {
+        currentQueue.push(currentNode.leftNode);
+      }
+      if (currentNode.rightNode) {
+        currentQueue.push(currentNode.rightNode);
+      }
+
+      levelOrderRecursive(currentQueue);
+    };
+
+    // const levelOrderIterative = (currentQueue) => {
+    //   while (currentQueue.length !== 0) {
+    //     const currentNode = currentQueue.shift();
+
+    //     if (callBack) {
+    //       callBack(currentNode);
+    //     } else {
+    //       levelOrderNodeList.push(currentNode.data);
+    //     }
+
+    //     if (currentNode.leftNode) {
+    //       currentQueue.push(currentNode.leftNode);
+    //     }
+    //     if (currentNode.rightNode) {
+    //       currentQueue.push(currentNode.rightNode);
+    //     }
+    //   }
+    // };
+
+    if (this.root !== null) {
+      levelOrderRecursive([this.root]);
+      // levelOrderIterative([this.root]);
+    }
+
+    return levelOrderNodeList.length > 0 ? levelOrderNodeList : null;
+  }
+
+  inOrder(callBack) {
+    const inOrderNodeList = [];
+
+    const inOrderRecursive = (currentNode) => {
+      if (currentNode === null) {
+        return;
+      }
+
+      inOrderRecursive(currentNode.leftNode);
+
+      if (callBack) {
+        callBack(currentNode);
+      } else {
+        inOrderNodeList.push(currentNode.data);
+      }
+
+      inOrderRecursive(currentNode.rightNode);
+    };
+
+    if (this.root !== null) {
+      inOrderRecursive(this.root);
+    }
+
+    return inOrderNodeList.length > 0 ? inOrderNodeList : null;
+  }
+
+  preOrder(callBack) {
+    const preOrderNodeList = [];
+
+    const preOrderRecursive = (currentNode) => {
+      if (currentNode === null) {
+        return;
+      }
+
+      if (callBack) {
+        callBack(currentNode);
+      } else {
+        preOrderNodeList.push(currentNode.data);
+      }
+
+      preOrderRecursive(currentNode.leftNode);
+      preOrderRecursive(currentNode.rightNode);
+    };
+
+    if (this.root !== null) {
+      preOrderRecursive(this.root);
+    }
+
+    return preOrderNodeList.length > 0 ? preOrderNodeList : null;
+  }
+
+  postOrder(callBack) {
+    const postOrderNodeList = [];
+
+    const postOrderRecursive = (currentNode) => {
+      if (currentNode === null) {
+        return;
+      }
+
+      postOrderRecursive(currentNode.leftNode);
+      postOrderRecursive(currentNode.rightNode);
+
+      if (callBack) {
+        callBack(currentNode);
+      } else {
+        postOrderNodeList.push(currentNode.data);
+      }
+    };
+
+    if (this.root !== null) {
+      postOrderRecursive(this.root);
+    }
+
+    return postOrderNodeList.length > 0 ? postOrderNodeList : null;
+  }
+
+  #findMinValue(node) {
+    let min = node.data;
+    let ptr = node;
+    while (ptr.leftNode !== null) {
+      min = ptr.leftNode.data;
+      ptr = ptr.leftNode;
+    }
+    return min;
+  }
+}
+
+export default Tree;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import "./css/normalize.css";
 import "material-icons/iconfont/round.css";
 import "./css/style.css";
 import footer from "./footerContent";
-import { Tree } from "./binarytTree";
+import { Tree } from "./binaryTree";
 
 // buildPageContent
 (() => {
   const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+  // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+  // const arr = [];
   const newBST = new Tree(arr);
   // console.log(newBST);
   const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -20,10 +22,17 @@ import { Tree } from "./binarytTree";
     }
   };
   newBST.insertNode(2);
-  newBST.insertNode(0);
-  // newBST.deleteNode(1);
-  // newBST.deleteNode(0);
-  console.log(newBST);
+  newBST.insertNode(-1);
+  // newBST.deleteNode(4);
   prettyPrint(newBST.root);
+  // console.log(newBST.find(99));
+  // newBST.levelOrder(console.log);
+  // console.log(newBST.levelOrder());
+  // newBST.inOrder(console.log);
+  console.log(newBST.inOrder());
+  // newBST.preOrder(console.log);
+  console.log(newBST.preOrder());
+  // newBST.postOrder(console.log);
+  console.log(newBST.postOrder());
   footer.buildFooter();
 })();
